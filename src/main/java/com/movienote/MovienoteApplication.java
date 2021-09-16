bda17bc08dfc90aeab47f567edef1aea602ebd13
AnastasiyaIlkevich <sirin199@yandex.ru>
2021-09-16 16:41:07 +0300

    MVN-2 (Create base spring boot starter app)

---
+++ src/main/java/com/movienote/MovienoteApplication.java
@@ -0,0 +1,13 @@
+package com.movienote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovienoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovienoteApplication.class, args);
+    }
+
+}
