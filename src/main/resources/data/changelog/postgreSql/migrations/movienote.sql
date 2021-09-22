51758be43c17d3ca09415347e9121631e3efdb90
Nadya Penkrat <nadya.penkrat@gmail.com>
2021-09-22 16:20:53 +0300

    MVN-1 (Create DB schema movienote.sql)

--- src/main/resources/data/changelog/postgreSql/migrations/movienote.sql
+++ src/main/resources/data/changelog/postgreSql/migrations/movienote.sql
@@ -2,19 +2,19 @@ DROP SCHEMA IF EXISTS movienote;
 
 
 CREATE SCHEMA IF NOT EXISTS movienote;
-USE movienote.public ;
+
 
 CREATE TABLE IF NOT EXISTS users(
-    id       BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id       SERIAL PRIMARY KEY NOT NULL,
     username VARCHAR(255)       NOT NULL UNIQUE,
     email    VARCHAR(255)       NOT NULL UNIQUE,
     password VARCHAR(255)       NOT NULL
-) ENGINE = InnoDB;
+);
 
 CREATE TABLE IF NOT EXISTS roles(
-    id   BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name VARCHAR(225)       NOT NULL
-) ENGINE = InnoDB;
+    id       SERIAL PRIMARY KEY NOT NULL,
+    name     VARCHAR(225)       NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS user_roles(
     user_id BIGINT,
@@ -22,28 +22,28 @@ CREATE TABLE IF NOT EXISTS user_roles(
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (role_id) REFERENCES roles (id)
         ON DELETE CASCADE
-) ENGINE = InnoDB;
+);
 
 CREATE TABLE IF NOT EXISTS status(
-    id     BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id    SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(245)       NOT NULL
-) ENGINE = InnoDB;
+);
 
 CREATE TABLE IF NOT EXISTS genres(
-    id          BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id          SERIAL PRIMARY KEY NOT NULL,
     name        VARCHAR(245)       NOT NULL UNIQUE,
     external_id BIGINT             NOT NULL
-) ENGINE = InnoDB;
+);
 
 CREATE TABLE IF NOT EXISTS movies(
-    id                BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id                SERIAL PRIMARY KEY NOT NULL,
     adult             BOOLEAN            NOT NULL,
-    poster            BLOB               NOT NULL,
+    poster            BYTEA               NOT NULL,
     original_language VARCHAR(45)        NOT NULL,
     original_title    VARCHAR(1245)      NOT NULL,
     overview          VARCHAR(1245)      NOT NULL,
     external_id       BIGINT             NOT NULL
-) ENGINE = InnoDB;
+);
 
 CREATE TABLE IF NOT EXISTS movie_genres(
     movie_id BIGINT NOT NULL,
@@ -51,10 +51,10 @@ CREATE TABLE IF NOT EXISTS movie_genres(
     FOREIGN KEY (movie_id) REFERENCES movies (id),
     FOREIGN KEY (genre_id) REFERENCES genres (id)
         ON DELETE CASCADE
-) ENGINE = InnoDB;
+);
 
 CREATE TABLE IF NOT EXISTS user_movies(
-    id                BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id                SERIAL PRIMARY KEY NOT NULL,
     user_id           BIGINT              NOT NULL,
     description        TEXT               NULL,
     rating             INT                NULL,
@@ -66,4 +66,4 @@ CREATE TABLE IF NOT EXISTS user_movies(
     FOREIGN KEY (status_id) REFERENCES status (id),
     FOREIGN KEY (movie_id) REFERENCES movies (id)
         ON DELETE CASCADE
-) ENGINE = InnoDB;
+);
